f66af01e18e1be6d202170f3fa53e87de90de0ed
Daan van Hasselt <contact@daanvanhasselt.com>
2023-09-10 12:28:39 +0200

    added theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { register } from './core/service-worker/serviceWorkerRegistration'
 import { GlobalStyles } from './styles'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './styles'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <div>hello world</div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <div>hello world</div>
+    </ThemeProvider>
   </React.StrictMode>
 )
 

20cd2bad5eec1599d9035a775afbaf9d5c932452
Daan van Hasselt <contact@daanvanhasselt.com>
2023-09-29 19:11:42 +0200

    Updates

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ try {
 
       // Manual injection point for manifest files.
       try {
-        workbox.precaching.precacheAndRoute([{"revision":"8f4823530449fb3a17a1dcc050871dd2","url":"index.html"},{"revision":"33dbdd0177549353eeeb785d02c294af","url":"logo192.png"},{"revision":"917515db74ea8d1aee6a246cfbcc0b45","url":"logo512.png"},{"revision":"80e64ce8a73bd5f6c13784ec19f454f0","url":"static/js/main.01694636.js"}])
+        workbox.precaching.precacheAndRoute([{"revision":"c5849d5c03109c97633bf652a77f14d7","url":"index.html"},{"revision":"33dbdd0177549353eeeb785d02c294af","url":"logo192.png"},{"revision":"917515db74ea8d1aee6a246cfbcc0b45","url":"logo512.png"},{"revision":"bea594a78b4dbbcbad561ce3a639f9e5","url":"static/js/main.eb499ea9.js"}])
       } catch (e) {
         console.error(e)
       }
